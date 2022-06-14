908320459ad4a3e6d0e725729fa6f8d6b14b374c
Sonia <1737438545@qq.com>
2022-06-14 22:11:20 +0800

    first

---
+++ Deepcopy/deepcopy2.js
@@ -0,0 +1,18 @@
+const deepClone = (a, cache) => {
+    if (!cache) {
+        cache = new Map() // 缓存不能全局，最好临时创建并递归传递
+    }
+    if (a instanceof Object) { // 不考虑跨 iframe
+        if (cache.get(a)) { return cache.get(a) }
+        let result
+        if (a instanceof Function) {
+            if (a.prototype) { // 有 prototype 就是普通函数
+                result = function () { return a.apply(this, arguments) }
+            } else {
+                result = (...args) => { return a.call(undefined, ...args) }
+            }
+        }
+    } else {
+        return a
+    }
+}
