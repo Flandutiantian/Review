30712f9c1706a6bbd19e6307bab5ff579b63ee27
Sonia <1737438545@qq.com>
2022-06-17 22:37:24 +0800

    div

---
+++ div.js
@@ -0,0 +1,23 @@
+< !DOCTYPE html >
+    <html>
+        <head>
+            <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width">
+                    <title>JS Bin</title>
+                </head>
+                <body>
+                    <div id="xxx"></div>
+                    <script> var dragging = false
+                        var position = null
+                        xxx.addEventListener('mousedown',function(e){
+                            dragging = true 
+                            position = [e.clientX, e.clientY] 
+                            })
+
+
+                        document.addEventListener('mouseup', function(e){
+                            dragging = false
+                        })
+                    </script>
+                </body>
+            </html>
